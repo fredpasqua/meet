f7ea4c4d04ecf8680e3333a46b11d0389e7d61c2
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-05 08:49:46 -0400

    Add test for CitySearch; refactored code in App.test.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 function App() {
   return (
     <div className="App">
-   <EventList />
+      <CitySearch />
+      <EventList />
     </div>
   );
 }
