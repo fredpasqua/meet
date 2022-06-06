fddad6c0b33c891cf551f6c8d14573a3d5f623ca
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-05 23:39:50 -0400

    save development docs

---
+++ src/eventbackup.js
@@ -0,0 +1,8 @@
+{
+  /* <p className="eventDateTime">{`${event.start.dateTime} +  " (" + ${event.start.timeZone} + ")"`}</p>
+                    <p className="etag-location1">{`${event.etag} + " (" + ${event.location} + ")"`}</p>
+                    <p className="about-event2">About event:</p>
+                    <a href="www.googel.com2">See details on Google Calendar</a>
+                    <p className="event-description2">{event.description}</p>
+                    <button className="details-toggle-button1"></button> */
+}
