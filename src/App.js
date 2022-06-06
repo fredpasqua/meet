fac3a0794ad8d162ed5a1432914e499a3c938685
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-05 23:39:03 -0400

    Event.js build

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import { mockData } from "./mock-data";
 function App() {
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={mockData} />
     </div>
   );
 }
--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,60 @@
-import React , { Component } from "react";
+import React, { Component } from "react";
 
 class Event extends Component {
-    render() {
-        return <div></div>;
-    }
-}
+  state = {
+    basicView: true,
+    detailView: false,
+  };
 
+  showDetails = () => {
+    this.setState({
+      basicView: false,
+      detailView: true,
+    });
+  };
+
+  hideDetails = () => {
+    this.setState({
+      basicView: true,
+      detailView: false,
+    });
+  };
+  render() {
+    const { event } = this.props;
+    return (
+      <>
+        <div className="event-basics">
+          <h2 className="event-title">{event.summary}</h2>
+          <p className="eventDateTime">
+            {`${event.start.dateTime} (${event.start.timeZone})`}
+          </p>
+          <p className="location">{`@${event.summary} | ${event.location}`}</p>
+          {this.state.basicView ? (
+            <button
+              onClick={this.showDetails}
+              className="details-toggle-button1"
+            >
+              show details
+            </button>
+          ) : null}
+        </div>
+        <div className="event-details">
+          {this.state.detailView ? (
+            <>
+              <p className="about-event">About event:</p>
+              <a href={event.htmlLink}>See details on Google Calendar</a>
+              <p className="event-description">{event.description}</p>
+              <button
+                onClick={this.hideDetails}
+                className="details-toggle-button1"
+              >
+                hide details
+              </button>
+            </>
+          ) : null}
+        </div>
+      </>
+    );
+  }
+}
 export default Event;
