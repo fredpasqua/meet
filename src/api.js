59f3c7aa1c84f82a1545cdfe7ba9423b49502718
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-09 19:56:54 -0400

    create getEvents Function

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import { mockData } from "./mock-data";
 
 //  @param {*} events:
 //  * The following function should be in the “api.js” file.
@@ -5,9 +6,12 @@
 //  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
 //  * The Set will remove all duplicates from the array.
 //  */
- export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-  };
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
 
+export const getEvents = async () => {
+  return mockData;
+};
