acc04ae8e508fa321b58c80b0920815c3615931f
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-18 14:55:46 -0400

    remove test

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,20 +3,22 @@ import NumberOfEvents from "../NumberOfEvents";
 import { shallow, mount } from "enzyme";
 
 describe("<NumberOfEvents /> component", () => {
-  let NumberOfEventsWrapper;
+                                                 let NumberOfEventsWrapper;
 
-  beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
-  });
+                                                 beforeAll(() => {
+                                                   NumberOfEventsWrapper = shallow(
+                                                     <NumberOfEvents />
+                                                   );
+                                                 });
 
-  test("render an input field for the number of events", () => {
-    expect(NumberOfEventsWrapper.find(".eventsToShow")).toHaveLength(1);
-  });
-
-  test("render 12 events to start", () => {
-    expect(NumberOfEventsWrapper.state("eventsToShow")).toEqual(12);
-  });
-});
+                                                 test("render an input field for the number of events", () => {
+                                                   expect(
+                                                     NumberOfEventsWrapper.find(
+                                                       ".eventsToShow"
+                                                     )
+                                                   ).toHaveLength(1);
+                                                 });
+                                               });
 
 describe("<NumberOfEvents /> integration", () => {
   let NumberOfEventsWrapper;
