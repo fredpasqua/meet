329c2b5c3334a17841effcf75d02cfa96491d65f
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-23 20:44:06 -0400

    Add Atatus Browser Monitoring

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from "atatus-spa";
+atatus.config("0ce0a08119f24a27a72e247bcd17b457").install();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -21,3 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error("Test Atatus Setup"));
