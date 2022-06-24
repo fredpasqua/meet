b803d97db982e06846367fe7eba47b85a63d696a
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-06-24 16:21:33 -0400

    added Critical error for no events to show

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
-import { ErrorAlert } from "./Alert";
+import { ErrorAlert, CriticalAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     errorMsg: null,
+    errorMsg2: null,
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    value < 1 || value > 32
+    console.log(value);
+    !value
+      ? this.setState({
+          errorMsg2: "Enter a number from 1 to 32",
+          errorMsg: null,
+        })
+      : value < 1 || value > 32
       ? this.setState({
           errorMsg: "must be between 1-32 please",
+          errorMsg2: null,
         })
       : this.setState({
           errorMsg: null,
+          errorMsg2: null,
         });
     this.props.updateEvents(undefined, value);
   };
 
   render() {
-    const { errorMsg } = this.state;
+    const { errorMsg, errorMsg2 } = this.state;
     const { numberOfEvents } = this.props;
     return (
       <>
-        <label className="NumOfEventsTitle">Number of Events:</label>
+        <label className="NumOfEventsTitle">Number of Events to View:</label>
         <ErrorAlert text={errorMsg}></ErrorAlert>
-
+        <CriticalAlert text={errorMsg2}></CriticalAlert>
         <div className="eventInput">
           <input
             label="Number of events"
